c51878475afbe70a99f71f7a00043957fefdb908
kiart-tantasi <petch_rwb@hotmail.com>
2023-05-07 21:02:18 +0700

    try nested layouts

---
+++ app/1/2/3/layout.tsx
@@ -0,0 +1,18 @@
+import LayoutComponent from "@/components/LayoutComponent";
+
+async function getData() {
+  const host = "https://nextjs-13-kt.vercel.app";
+  const res = await fetch(`${host}/api/delay?time=6000`, {
+    cache: "no-store",
+  });
+  return res.json();
+}
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const data = await getData();
+  return <LayoutComponent>{children}</LayoutComponent>;
+}
---
+++ app/1/2/3/page.tsx
@@ -0,0 +1,17 @@
+async function getData() {
+  const host = "https://nextjs-13-kt.vercel.app";
+  const res = await fetch(`${host}/api/delay?time=8000`, {
+    cache: "no-store",
+  });
+  return res.json();
+}
+
+export default async function Page() {
+  const data = await getData();
+  return (
+    <div style={{ border: "1px solid black", textAlign: "center" }}>
+      <h1 style={{ textAlign: "center" }}>Page is successfully rendered !</h1>
+      <p style={{ fontSize: "10px" }}>page data: {JSON.stringify(data)}</p>
+    </div>
+  );
+}
