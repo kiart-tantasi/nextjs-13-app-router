81ee6be5f0e97ca50abc24dbaec0e4ab41852dc7
kiart-tantasi <petch_rwb@hotmail.com>
2023-05-08 08:28:57 +0700

    add try catch

--- app/api/delay/route.ts
+++ app/api/delay/route.ts
@@ -1,14 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const paramsString: string | undefined = req.url.split("?")[1];
-  const params: URLSearchParams = new URLSearchParams(paramsString);
-  const queryParamTime: string | null = params.get("time");
-  let timeToDelay: number = queryParamTime ? +queryParamTime : 2000;
-  const MAX_TIME_ALLOWED = 10_000;
-  timeToDelay = timeToDelay > MAX_TIME_ALLOWED ? MAX_TIME_ALLOWED : timeToDelay;
-  await new Promise((res) => {
-    setTimeout(res, timeToDelay);
-  });
-  return NextResponse.json({ message: "hello world" });
+  try {
+    const paramsString: string | undefined = req.url.split("?")[1];
+    const params: URLSearchParams = new URLSearchParams(paramsString);
+    const queryParamTime: string | null = params.get("time");
+    let timeToDelay: number = queryParamTime ? +queryParamTime : 2000;
+    const MAX_TIME_ALLOWED = 10_000;
+    timeToDelay =
+      timeToDelay > MAX_TIME_ALLOWED ? MAX_TIME_ALLOWED : timeToDelay;
+    await new Promise((res) => {
+      setTimeout(res, timeToDelay);
+    });
+    return NextResponse.json({ message: "hello world" });
+  } catch (e) {
+    return NextResponse.json({ message: (e as Error).toString() });
+  }
 }
