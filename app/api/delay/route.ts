dd04a5dfde819cfa21d95f58c00964579c880e11
kiart-tantasi <petch_rwb@hotmail.com>
2023-05-08 09:29:45 +0700

    remove try catch from /api/delay

--- app/api/delay/route.ts
+++ app/api/delay/route.ts
@@ -1,19 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  try {
-    const paramsString: string | undefined = req.url.split("?")[1];
-    const params: URLSearchParams = new URLSearchParams(paramsString);
-    const queryParamTime: string | null = params.get("time");
-    let timeToDelay: number = queryParamTime ? +queryParamTime : 2000;
-    const MAX_TIME_ALLOWED = 10_000;
-    timeToDelay =
-      timeToDelay > MAX_TIME_ALLOWED ? MAX_TIME_ALLOWED : timeToDelay;
-    await new Promise((res) => {
-      setTimeout(res, timeToDelay);
-    });
-    return NextResponse.json({ message: "hello world" });
-  } catch (e) {
-    return NextResponse.json({ message: (e as Error).toString() });
-  }
+  const paramsString: string | undefined = req.url.split("?")[1];
+  const params: URLSearchParams = new URLSearchParams(paramsString);
+  const queryParamTime: string | null = params.get("time");
+  let timeToDelay: number = queryParamTime ? +queryParamTime : 2000;
+  const MAX_TIME_ALLOWED = 10_000;
+  timeToDelay = timeToDelay > MAX_TIME_ALLOWED ? MAX_TIME_ALLOWED : timeToDelay;
+  await new Promise((res) => {
+    setTimeout(res, timeToDelay);
+  });
+  return NextResponse.json({ message: "hello world" });
 }
