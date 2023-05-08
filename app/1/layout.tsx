1db2253bb2bbe954c6df5ca8efe7732fa7a1627b
kiart-tantasi <petch_rwb@hotmail.com>
2023-05-08 09:33:08 +0700

    apply getApiUrl to all pages

--- app/1/layout.tsx
+++ app/1/layout.tsx
@@ -1,8 +1,8 @@
 import LayoutComponent from "@/app/@components/LayoutComponent";
+import getApiUrl from "../@utils/getApiUrl";
 
 async function getData() {
-  const host = "https://nextjs-13-kt.vercel.app";
-  const res = await fetch(`${host}/api/delay?time=2000`, {
+  const res = await fetch(getApiUrl("/api/delay?time=2000"), {
     cache: "no-store",
   });
   return res.json();
--- app/stream-ui/layout.tsx
+++ app/stream-ui/layout.tsx
@@ -2,7 +2,6 @@ import LayoutComponent from "@/app/@components/LayoutComponent";
 import getApiUrl from "../@utils/getApiUrl";
 
 async function getData() {
-  const host = "https://nextjs-13-kt.vercel.app";
   const res = await fetch(getApiUrl("/api/delay?time=2000"), {
     cache: "no-store",
   });
