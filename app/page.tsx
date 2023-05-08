f9bc544f914ec49909f6418ac8dd7d8a8ffb3e18
kiart-tantasi <petch_rwb@hotmail.com>
2023-05-08 09:19:08 +0700

    refactor home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,14 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div>
-      <Link href="/stream-ui">
-        <h1>Try Streaming UI</h1>
-      </Link>
+      <a href="/stream-ui">
+        <b>Try Streaming UI</b>
+      </a>
+      <br />
+      <br />
+      <a href="/1/2/3">
+        <b>Try Streaming UI 2 (triple nested layouts)</b>
+      </a>
     </div>
   );
 }
